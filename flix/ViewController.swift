48b0bd832dc0fafaf2c92066d1933cea257b2dcf
Farel Harimawan <farel@farels-macbook-pro.local>
2019-04-13 01:07:14 -0700

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Farel Harimawan on 4/13/19.
+//  Copyright © 2019 fharimaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
